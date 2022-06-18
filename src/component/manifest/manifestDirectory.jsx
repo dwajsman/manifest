3614465ff3daf7ad16dbd714b49ba6c8b9506b38
Daniel Wajsman <danielwajsman@mac.com>
2022-06-18 15:58:59 -0700

    manifest structure fixed

--- src/component/manifest/manifestDirectory.jsx
+++ src/component/manifest/manifestDirectory.jsx
@@ -3,10 +3,56 @@ import ManifestItem from './manifestItem';
 
 
 export default function manifestDirectory() {
+
+  const manifests = [
+    { 
+      id: 393988282288282,
+      manifest: 'The journey of a thousand miles begins with one step.',
+      postedTime: '10 mins ago',
+      user: {
+        name: 'Lao Tzu',
+        photo: 'https://placekitten.com/80/80',
+        id: 22331
+      },
+      feedback: {
+        star: true,
+        spark: false,
+        like: false,
+        heart: true,
+      }
+    }, 
+    {
+      id: 393988282288283,
+      manifest: 'That which does not kill us makes us stronger.',
+      postedTime: '10 mins ago',
+      user: {
+        name: 'Friedrich Nietzsche',
+        photo: 'https://placekitten.com/80/80',
+        id: 22333
+      },
+      feedback: {
+        star: true,
+        spark: false,
+        like: false,
+        heart: true,
+      }
+    }
+
+  ]
+
   return (
     <div>
       <h2>manifestDirectory</h2>
-      <ManifestItem />
+
+
+      {manifests.map((manifest)=>{
+        return (
+          <ManifestItem manifest={manifest} key={manifest.id} />
+
+        )
+      })
+      } 
+      
     </div>
 
   )
--- src/component/manifest/manifestItem.jsx
+++ src/component/manifest/manifestItem.jsx
@@ -1,49 +1,12 @@
 import React from 'react'
+import User from '../ux-elements/user';
 
-export default function manifestItem(props) {
-
-  const manifests = [
-    { 
-      id: 393988282288282,
-      manifest: 'The journey of a thousand miles begins with one step.',
-      postedTime: '10 mins ago',
-      user: {
-        name: 'Lao Tzu',
-        photo: 'https://placekitten.com/80/80',
-        id: 22331
-      }
-    }, 
-    {
-      id: 393988282288283,
-      manifest: 'That which does not kill us makes us stronger.',
-      postedTime: '10 mins ago',
-      user: {
-        name: 'Friedrich Nietzsche',
-        photo: 'https://placekitten.com/80/80',
-        id: 22333
-      }
-
-    }
-
-  ]
-
+export default function manifestItem({manifest}) {
 
   return (
     <div>
-      {manifests.map((m)=>{
-        return (
-          <div key={m.id}>
-            <p>{m.manifest}</p>
-            <User user={m.user}/>
-            
-          </div>
-        )
-      })
-      }
-    
-    
-
-
-  </div>
+      <p>{manifest.manifest}</p>
+      <User user={manifest.user}/>
+    </div>
   )
 }
